903b23dae00e6449596d0fd8ee629a6637704871
alvarobernal2412 <alvarobc2412@gmail.com>
2026-06-08 13:52:21 +0200

    test: patch marimo ast_compile in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,8 @@
 import os
 os.environ.setdefault("MPLBACKEND", "Agg")
+
+try:
+    from tracker.kernel_hooks import patch_marimo_ast_compile
+    patch_marimo_ast_compile()
+except Exception:
+    pass
